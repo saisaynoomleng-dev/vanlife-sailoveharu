9ae973056a85e93496377a1b49b239f4babc5d13
saisaynoomleng-dev <snoomleng1@cougars.ccis.edu>
2025-07-25 12:39:30 -0500

    finished about page

--- src/app/(web)/about/page.tsx
+++ src/app/(web)/about/page.tsx
@@ -1,7 +1,47 @@
-import React from 'react';
+import Bounded from '@/components/Bounded';
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const AboutPage = () => {
-  return <div>AboutPage</div>;
+  return (
+    <Bounded className="min-h-[86vh]">
+      <Image
+        width={768}
+        height={300}
+        src="/about-hero.png"
+        alt="a man sitting on the roof of the van looking at the sky full of stars"
+      />
+
+      <p className="font-bold text-fs-700 leading-8">
+        Don&apos;t squeeze in a sedan when you could relax in a van.
+      </p>
+
+      <p>
+        Our mission is to enliven your road trip with the perfect travel van
+        rental. Our vans are recertified before each trip to ensure your travel
+        plans can go off without a hitch. (Hitch costs extra)
+      </p>
+
+      <p>
+        Our team is full of vanlife enthusiasts who know firsthand the magic of
+        touring the world on 4 wheels.
+      </p>
+
+      <div className="bg-brand-orange-300 px-3 py-5 flex justify-center flex-col gap-3">
+        <p className="font-bold text-fs-500">Your destination is waiting.</p>
+        <p className="font-bold text-fs-500">Your van is ready.</p>
+        <Link href="/vans">
+          <Button
+            variant="link"
+            className="bg-brand-black-200 text-brand-white-100"
+          >
+            Explore our vans
+          </Button>
+        </Link>
+      </div>
+    </Bounded>
+  );
 };
 
 export default AboutPage;
